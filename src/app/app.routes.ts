6b988f76eb3cc7edf928f498ef48d1eb05b14e82
HarshaDeepRam <vanga.ram@coforge.com>
2024-12-05 14:57:10 +0530

    Final Commit 1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PipesdemoComponent } from './pipesdemo/pipesdemo.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { DetailsComponent } from './details/details.component';
+import { TeamComponent } from './team/team.component';
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'emp', component: EmployeeComponent},
+    {path: 'about', component: AboutusComponent},
+    {path: 'pipes', component: PipesdemoComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'det', component: DetailsComponent},
+    {path: 'team', component: TeamComponent}
+];
